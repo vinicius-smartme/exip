a732172c6fe99a543a1fce2e0b7700860149cd93
vinicius-smartme <vinicius.franchini@smart-me.com>
2023-08-30 10:42:57 +0200

    code_utils: Add 'List' single linked list
    Add a single linked list and functions to use it.

--- include/codec_utils.h
+++ include/codec_utils.h
@@ -14,6 +14,31 @@
 #define CODE_UTILS_H_
 
 #include "procTypes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    void *data;
+    size_t size;
+    struct Node *next;
+} Node;
+
+typedef struct List {
+    Node *head;
+    Node *tail;
+    int size;
+} List;
+
+List new_list();
+void push_back(List *list, void *data, size_t size);
+Node* pop_front(List *list);
+Node* pop_at(List *list, int index);
+Node* getNth(List *list, int index);
+void print_list(List *list);
+void clear_node(Node *node);
+void delete_list(List *list);
+List* copy_list(List* list);
 
 EXIPSchema parseSchema(char *xsdList);
 /*void parseOpsMask(char* mask, EXIOptions* ops);
